27471d847559077ae2b7a6bb2c85714ed9faa7b0
ohmsl <ollie@nexdynamic.com>
2024-02-18 18:38:47 +0000

    tsdb concept

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,8 @@
-
-
-
-pub mod tsdb;
+mod tsdb;
+use tsdb::tsdb;
 
 fn main() {
-    const TEST : &str = test();
-
-    let message = hey(TEST.to_string());
-    let message = message.to_uppercase();
-    let message = message.replace("WORLD", "RUST");
-    
-    println!("{}", message);
-}
-
-const fn test () -> &'static str {
-    "HELLO WORLD"
-}
-
-fn hey(p: String) -> String { 
-    if 1 == 1 {
-        format!("HELLO {p} WORLD")
-    } else {
-        format!("GOODBYE WORLD {p}")
-    }
+    //println!("Hello, world!");
+    //print!("Hello, world!"); doesnt print anything on a new line unless you use \n.
+    tsdb(); // runs the tsdb function from tsdb.rs
 }
