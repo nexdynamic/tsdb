5817ab01e172d09dc8d83df8980188257517dd29
Theo Slater <theoslater1@gmail.com>
2025-07-23 14:46:18 +0100

    define datatypes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-mod saving;
+
 mod tsdb;
-//use tsdb::tsdb;
+use tsdb::tsdb;
 
 fn main() {
-    //tsdb(); // disabled for to test new file saving systme.
-    if let Err(e) = saving::saving() {
-        println!("An error occurred: {}", e);
-    }
+    tsdb();
 }
--- src/tsdb.rs
+++ src/tsdb.rs
@@ -1,44 +1,27 @@
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
+use std::{path::PathBuf, ptr::null};
 
-#[derive(Serialize, Deserialize)]
-pub struct DataPoint {
-    timestamp: DateTime<Utc>,
-    value: i32, // this uses integers for the data and not floats
-}
-#[derive(Serialize, Deserialize)]
-pub struct TsDb {
-    data: Vec<DataPoint>,
+struct  TimeSeriesPoint {
+    timestamp: u64,
+    value: f64,
 }
 
-impl TsDb {
-    pub fn new() -> Self {
-        TsDb { data: Vec::new() }
-    }
-
-    pub fn insert(&mut self, value: i32) {
-        self.data.push(DataPoint {
-            timestamp: Utc::now(),
-            value,
-        });
-    }
-
-    pub fn query_all(&self) -> &[DataPoint] {
-        &self.data
-    }
+struct ChunkIndex {
+    start_time: u64,
+    end_time: u64,
+    file_offset: u64, // where we find the chunk on dixsk
 }
+
+pub struct ChunkWriter {
+    buffer: Vec<TimeSeriesPoint>,
+    max_points: usize,
+    chunk_dir: PathBuf
+}
+
+// TODO: buffer data here.
+// we'll try flushing it after t seconds
+
 
 pub fn tsdb() {
-    let mut tsdb = TsDb::new();
-    //prints the retrievead data to the console.
-    tsdb.insert(10);
-    tsdb.insert(20);
-    tsdb.insert(30);
-
-    for data_point in tsdb.query_all() {
-        println!(
-            "Timestamp: {}, Value: {}",
-            data_point.timestamp, data_point.value
-        );
-    }
+    
 }
+  
